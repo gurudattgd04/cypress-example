3cb724fa3bd0db00c8e54e9b2885aa76e27ea980
gurudattsa <gurudatt.gd04@gmail.com>
2022-08-26 11:52:19 +0530

    updated code

---
+++ cypress/e2e/dropdown-examples/dropdown-test.spec.js
@@ -0,0 +1,28 @@
+it.only("Validate the dropdown option selection by it's index", () => {
+  cy.visit("https://www.bstackdemo.com/");
+  cy.get("select").select(1).invoke("val").should("eq", "lowestprice");
+});
+
+it.only("Validate the dropdown option selection by it's value", () => {
+  cy.visit("https://www.bstackdemo.com/");
+  cy.get("select")
+    .select("lowestprice")
+    .invoke("val")
+    .should("eq", "lowestprice");
+});
+
+it.only("Validate the dropdown option selection by it's text", () => {
+  cy.visit("https://www.bstackdemo.com/");
+  cy.get("select")
+    .select("Highest to lowest")
+    .invoke("val")
+    .should("eq", "highestprice");
+});
+
+it.only("Validate the dropdown option selection by it's text and assert the selection", () => {
+  cy.visit("https://www.bstackdemo.com/");
+  cy.get("select").select("Highest to lowest");
+  cy.get("select option:selected")
+    .invoke("text")
+    .should("eq", "Highest to lowest");
+});
